176465e31db4aab1dc6a127610f1c7920d3d55bf
Leandro Carlos <melo.leandro@estudante.ufjf.br>
2023-11-26 18:28:32 -0300

    conserta texto do código

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import {getJoao, getMaria, deJoaoParaMaria, deMariaParaJoao, setJoao, setMaria} 
 let maria = getMaria();
 let joao = getJoao();
 
-console.log(`João tem ${joao} maçãs e Maria tem ${maria} maçãs.`)
+console.log(`João tem ${joao} maçã e Maria tem ${maria} maçãs.`)
 
 deMariaParaJoao();
 maria = getMaria();
 joao = getJoao();
 
-console.log(`Porém, João era um ladrão, e roubou todas as maçãs de Maria, e fugiu com ${joao} maçãs. Maria, triste, acabou com ${maria} maçãs`);
+console.log(`Porém, João era um ladrão, e roubou todas as maçãs de Maria, e fugiu com ${joao} maçãs. Maria, triste, acabou com ${maria} maçãs.`);
 
