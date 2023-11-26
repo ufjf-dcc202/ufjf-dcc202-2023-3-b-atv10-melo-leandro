576e19695ac508d02f46ca29c1b2b320b8fa60ff
Leandro Carlos <melo.leandro@estudante.ufjf.br>
2023-11-26 18:12:46 -0300

    cria funções para manipulação de "joão" e "maria"

---
+++ src/joaoEmaria.js
@@ -0,0 +1,36 @@
+let joao = 1;
+let maria = 2;
+
+function getJoao() {
+    return joao;
+}
+
+function getMaria() {
+    return maria;
+}
+
+function setJoao (valor){
+    if (valor > 0)
+        joao = valor;
+    else
+        joao = 0;
+}
+
+function setMaria (valor){
+    if (valor > 0)
+        maria = valor;
+    else
+        maria = 0;
+}
+
+function deJoaoParaMaria(){
+    setMaria(joao + maria);
+    setJoao(0);
+}
+
+function deMariaParaJoao(){
+    setJoao(joao + maria);
+    setMaria(0);
+}
+
+export{getJoao, getMaria, setJoao, setMaria, deJoaoParaMaria, deMariaParaJoao}
